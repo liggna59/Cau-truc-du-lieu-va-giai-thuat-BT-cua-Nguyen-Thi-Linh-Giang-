25237f58e05da0547e36461cb566fdad608be38b
liggna59 <linhgiang5995@gmail.com>
2026-04-27 10:56:48 +0700

    chen file theo tgian

--- bttuan6.cpp
+++ bttuan6.cpp
@@ -11,6 +11,7 @@ struct Node{
     File data;
     Node* next;
 };
+//Tạo node mới
 Node* taoNodemoi(string ten, double kthuocteptin, int moctgian){
     Node* newNode = new Node;
     newNode ->data.ten = ten;
@@ -19,3 +20,19 @@ Node* taoNodemoi(string ten, double kthuocteptin, int moctgian){
     newNode->next = nullptr;
     return newNode;
 }
+// Chèn file thoi tgian
+void chentheotgian(Node*& head, string ten, double kthuocteptin, int moctgian){
+    Node* newNode = taonodemoi(ten, kthuocteptin, moctgian);
+    if(head==nullptr || head->data.moctgian>=newNode->data.moctgian){
+        newNode->next = head;
+        head=NewNode;
+        return;
+    }
+    Node*htai=head;
+    while (htai->next != nullptr&& htai->next->data.moctgian<newNode->data.moctgian){
+        htai=htai->next;
+    }
+    newNode->next=htai->next;
+    htai->next=newNode;
+}
+//Tính tổng dung lượng của các file hiện có 
