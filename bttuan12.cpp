43f121cefb82cbc63eff5052b422985087844d1b
liggna59 <linhgiang5995@gmail.com>
2026-05-27 22:13:23 +0700

    ham vun dong tai 1 node

--- bttuan12.cpp
+++ bttuan12.cpp
@@ -9,3 +9,20 @@ void intrangthaimang(const vector<int>& mangtruyenvao) {
     }
     cout << "]" << endl;
 }
+// Hàm vun đống tại một node
+void vundong(vector<int>& mangdangxet, int n, int i) {
+    int lonnhat = i;
+    int trai = 2 * i + 1;
+    int phai = 2 * i + 2;
+    if (trai < n && mangdangxet[trai] > mangdangxet[lonnhat]) {
+        lonnhat = trai;
+    }
+    if (phai < n && mangdangxet[phai] > mangdangxet[lonnhat]) {
+        lonnhat = phai;
+    }
+    if (lonnhat != i) {
+        swap(mangdangxet[i], mangdangxet[lonnhat]);
+        // Vun đống lại đệ quy
+        vundong(mangdangxet, n, lonnhat);
+    }
+}
