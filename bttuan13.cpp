9bf44c6af9eb9c2196fb406a02efb3291affb92c
liggna59 <linhgiang5995@gmail.com>
2026-06-01 22:44:49 +0700

    Cai dat ham them va timkiem

--- bttuan13.cpp
+++ bttuan13.cpp
@@ -12,3 +12,33 @@ struct node {
         phai = NULL;
     }
 };
+// Ham them gia tri vao cay nhi phan tim kiem
+node* them(node* goc, int x) {
+    if (goc == NULL) {
+        return new node(x);
+    }
+    if (x <= goc->gtri) {
+        goc->trai = them(goc->trai, x);
+    } else {
+        goc->phai = them(goc->phai, x);
+    }
+    return goc;
+}
+// Ham tim kiem 
+bool timkiem(node* goc, int x) {
+    if (goc == NULL) {
+        cout << " -> NULL (Khong tim thay)" << endl;
+        return false;
+    }
+    cout << goc->gtri;
+    if (goc->gtri == x) {
+        cout << " (Da tim thay gtri can tim)" << endl;
+        return true;
+    }
+    cout << " -> ";
+    if (x <= goc->gtri) {
+        return timkiem(goc->trai, x);
+    } else {
+        return timkiem(goc->phai, x);
+    }
+}
